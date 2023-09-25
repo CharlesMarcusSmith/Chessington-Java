7961e3d144e29430c9ce6e574096da23482f1bfc
CharlesMarcusSmith <charliemarcussmith@googlemail.com>
2023-09-25 16:01:06 +0100

    CS: Red-2 Pawn tests passed completed.

--- app/src/main/java/chessington/model/pieces/Pawn.java
+++ app/src/main/java/chessington/model/pieces/Pawn.java
@@ -26,12 +26,23 @@ public class Pawn extends AbstractPiece {
     public List<Move> allowedWhiteMoves(Coordinates from, Board board){
         List<Move> allowedMoves = new ArrayList<>();
 
+        //Coordinate objects to see potential move squares are populated with other pieces
         Coordinates moveOneAhead = from.plus(-1, 0);
+        Coordinates moveTwoAhead = from.plus(-2, 0);
+
 
         if(board.get(moveOneAhead) == null){
-            Move move = new Move(from, moveOneAhead);
-            allowedMoves.add(move);
+            Move moveOne = new Move(from, moveOneAhead);
+            allowedMoves.add(moveOne);
+
+            //2 Moves on first move:
+            if(board.get(moveTwoAhead) == null  && from.getRow() == 6){
+                Move moveTwo = new Move(from, moveTwoAhead);
+                allowedMoves.add(moveTwo);
+            }
         }
+
+
         return allowedMoves;
     }
 
@@ -39,11 +50,19 @@ public class Pawn extends AbstractPiece {
         List<Move> allowedMoves = new ArrayList<>();
 
         Coordinates moveOneAhead = from.plus(1, 0);
+        Coordinates moveTwoAhead = from.plus(2, 0);
 
         if(board.get(moveOneAhead) == null){
-            Move move = new Move(from, moveOneAhead);
-            allowedMoves.add(move);
+            Move moveOne = new Move(from, moveOneAhead);
+            allowedMoves.add(moveOne);
+
+            //2 Moves on first move:
+            if(board.get(moveTwoAhead) == null && from.getRow() == 1){
+                Move moveTwo = new Move(from, moveTwoAhead);
+                allowedMoves.add(moveTwo);
+            }
         }
+
         return allowedMoves;
     }
 }
